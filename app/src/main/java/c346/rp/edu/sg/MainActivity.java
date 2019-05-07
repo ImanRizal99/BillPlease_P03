2419c8e5a752f6089fe59efaede91c9560647313
Iman Rizal <17040236@myrp.edu.sg>
2019-05-07 12:18:47 +0800

    Add textInput

--- app/src/main/java/c346/rp/edu/sg/MainActivity.java
+++ app/src/main/java/c346/rp/edu/sg/MainActivity.java
@@ -1,10 +1,7 @@
 package c346.rp.edu.sg;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
